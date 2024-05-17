d2f9ffa8cd963cf4921dbd4cb2fa262608ae327a
quangtn266 <quangtrandn93@gmail.com>
2024-05-17 14:02:19 +0700

    update schema and Mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+// connect to mlab database.
+// place mydb string & creds.
+// username:quangtrandn93  pass:
+mongoose.connect("mongodb+srv://quangtrandn93:<pass>@cluster0.ntpafby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connection.once('open',() => {
+    console.log('connected to database')
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
