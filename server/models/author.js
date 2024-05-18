eca8a223cfa5417e0aa61168a2f05c5481ea5bde
quangtn266 <quangtrandn93@gmail.com>
2024-05-18 09:57:29 +0700

    update mutation for Mongodb and app

--- server/models/author.js
+++ server/models/author.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const authorSchema = new Schem([
+const authorSchema = new Schema({
     name: String,
-    age: Number,
-]);
+    age: Number
+});
 
 module.exports = mongoose.model('Author', authorSchema);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -59,6 +59,42 @@ const AuthorType = new GraphQLObjectType({
     })
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString},
+                age: { type: GraphQLInt}
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+            });
+            return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString},
+                genre: { type: GraphQLString},
+                authorId: { type: GraphQLID}
+            },
+            resolve (parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+    }
+})
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:  {
@@ -97,5 +133,6 @@ const RootQuery = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation: Mutation
 });
